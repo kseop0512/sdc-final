cc4e65d63dd3a58ee11735e68bc4836f93c753d7
부가원 <bbookk1104@gmail.com>
2022-10-28 16:27:08 +0900

    221028(16:27)나의정보 수정 만드는 중...

--- src/main/java/kr/or/member/controller/MemberController.java
+++ src/main/java/kr/or/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package kr.or.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -45,4 +46,22 @@ public class MemberController {
 	public String memberMypage() {
 		return "member/memberMypage";
 	}
+	/*
+	@RequestMapping(value="/memberUpdate.do")
+	public String memberUpdate(Member m, Model model) {
+		int result = service.updateMember(m);
+		if(result>0) {
+			model.addAttribute("title","정보수정 완료");
+			model.addAttribute("msg","입력하신 정보로 수정을 완료했습니다.");
+			model.addAttribute("icon","success");
+			model.addAttribute("loc","/memberMypage.do");
+		}else {
+			model.addAttribute("title","정보수정 실패");
+			model.addAttribute("msg","정보수정 중 오류가 발생했습니다.");
+			model.addAttribute("icon","error");
+			model.addAttribute("loc","/memberMypage.do");
+		}
+		return "common/msg";
+	}
+	*/
 }
