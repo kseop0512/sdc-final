0299be6dd758a9d220a5db1bf2f54edf73b13e2c
91 <khrndnjs@gmail.com>
2022-11-17 15:07:50 +0900

    -마이페이지 css

--- src/main/java/kr/or/member/model/service/MessageService2.java
+++ src/main/java/kr/or/member/model/service/MessageService2.java
@@ -9,15 +9,19 @@ import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 @Service
 public class MessageService2 {
-	String api_key = "NCSFVKPMCPZOKMGN";
-	String api_secret = "GZ4O0EFQMVPYV3IISRXVHLETWGIRTRTY";
+	//내꺼
+	//String api_key = "NCSFVKPMCPZOKMGN";
+	//String api_secret = "GZ4O0EFQMVPYV3IISRXVHLETWGIRTRTY";
 	 
+	//혜규누나꺼
+	String api_key = "NCSMTTQ4FCRBKKN9";
+	String api_secret = "ICW2VA8P8JJLZDBWMSASXMRWEDIEJ0JH";
 	public void sendMessage(String pNum, String randomNumber) {
 		Message coolsms = new Message(api_key, api_secret);
 		 
 	    HashMap<String, String> params = new HashMap<String, String>();
 	    params.put("to", pNum);
-	    params.put("from", "01062379953");
+	    params.put("from", "01027850281");
 	    params.put("type", "SMS");
 	    params.put("text", "[똑독캣] 회원가입 인증번호는 ["+randomNumber+"]입니다.");
 	    params.put("app_version", "test app 1.2"); // application name and version
